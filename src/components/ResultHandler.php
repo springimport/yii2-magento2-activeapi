dfb741ca0a346f6321b6f3ad0d2a591ff9654b75
Mykhailo Shatilov <mykhailo@springimport.com>
2016-08-24 09:38:51 -0400

    fix

--- src/components/ResultHandler.php
+++ src/components/ResultHandler.php
@@ -60,8 +60,6 @@ class ResultHandler extends AbstractResultHandler
 
     protected function parseErrors()
     {
-        //echo \yii\helpers\VarDumper::dump($this->content, 10, true);exit;
-        
         if ($this->content && isset($this->content->message)) {
             if (isset($this->content->parameters)) {
                 $engine = new StringTemplate\Engine(
